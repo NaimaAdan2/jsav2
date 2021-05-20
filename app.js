393d08159a58f75bf4679a6b7f0b9ce52256393b
Suaad <suaadsaeed111@gmail.com>
2021-05-20 21:39:46 +0100

    change

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
 
 // Set up express server
 const app = express();
-// Use middleware to allow for json + form requests
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}));
 
 // Set static folder
 app.use(express.static('public'))
-app.use(express.static('src'))
-app.use(express.static('build'))
-app.use(express.static('lib'))
-
 
 const PORT = process.env.PORT || 8080;
 
