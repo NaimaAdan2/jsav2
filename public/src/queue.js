96a196f0f6b4c335b0394ccda748bca4eb68d2ce
Suaad <suaadsaeed111@gmail.com>
2021-05-21 02:03:20 +0100

    refactor

--- public/src/queue.js
+++ public/src/queue.js
@@ -1,3 +1,41 @@
+// Initialise JSAV container and control buttons
+let enqueueElement = document.getElementById("enqueue");
+let dequeueElement = document.getElementById("dequeue");
+let jsav = new JSAV("container");
+let jsavArr = jsav.ds.array([]);
+// Initialise JSAV pointers
+let headPointer;
+let tailPointer;
+
+// When a user clicks "enqueue" add new value to the array and update pointers
+enqueueElement.addEventListener("click", function() {
+    var input = document.getElementById("userInput")
+    var valueToAdd = input.value
+    jsavArr = enqueueArray(jsav, jsavArr, valueToAdd) 
+    if (headPointer) {
+        headPointer.hide()
+    }
+    if (tailPointer) {
+        tailPointer.hide()
+    }
+    tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+    headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
+})
+
+// When a user clicks "dequeue" remove value at tail of the array and update pointers
+dequeueElement.addEventListener("click", function() {
+    jsavArr = dequeueArray(jsav, jsavArr) 
+    if (headPointer) {
+        headPointer.hide()
+    }
+    if (tailPointer) {
+        tailPointer.hide()
+    }
+    tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+    headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
+})
+
+
 // Workaround since JSAV does not support adding to array datastructure
 function enqueueArray(jsav, jsavArray, value) {
     var newArr = jsavArray._values
@@ -15,45 +53,3 @@ function dequeueArray(jsav, jsavArray) {
     jsavArray.hide()
     return jsav.ds.array(newArr)
 }
-
-
-function initiliaseQueueVisualisation() {
-    // Initialise JSAV container and control buttons
-    let enqueueElement = document.getElementById("enqueue");
-    let dequeueElement = document.getElementById("dequeue");
-    let jsav = new JSAV("container");
-    let jsavArr = jsav.ds.array([]);
-    // Initialise JSAV pointers
-    let headPointer;
-    let tailPointer;
-
-    // When a user clicks "enqueue" add new value to the array and update pointers
-    enqueueElement.addEventListener("click", function() {
-        var input = document.getElementById("userInput")
-        var valueToAdd = input.value
-        jsavArr = enqueueArray(jsav, jsavArr, valueToAdd) 
-        if (headPointer) {
-            headPointer.hide()
-        }
-        if (tailPointer) {
-            tailPointer.hide()
-        }
-        tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
-        headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
-    })
-
-    // When a user clicks "dequeue" remove value at tail of the array and update pointers
-    dequeueElement.addEventListener("click", function() {
-        jsavArr = dequeueArray(jsav, jsavArr) 
-        if (headPointer) {
-            headPointer.hide()
-        }
-        if (tailPointer) {
-            tailPointer.hide()
-        }
-        tailPointer = jsav.pointer("tail", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
-        headPointer = jsav.pointer("Head", jsavArr.index(0), {arrowAnchor: "left top"})
-    })
-}
-
-initiliaseQueueVisualisation()
--- public/src/stack.js
+++ public/src/stack.js
@@ -1,3 +1,46 @@
+// Initialise JSAV container and control buttons
+let pushElement = document.getElementById("push");
+let popElement = document.getElementById("pop");
+let jsav = new JSAV("container");
+let jsavArr = jsav.ds.array([]);
+// Initialise JSAV pointers
+let headPointer;
+let tailPointer;
+
+// When a user clicks "push" add new value to the array and update pointers
+pushElement.addEventListener("click", function() {
+  let input = document.getElementById("userInput")
+  let valueToAdd = input.value
+  jsavArr = addToArray(jsav, jsavArr, valueToAdd)
+    
+    // Hide old pointers and create new pointers
+  if (headPointer) {
+    headPointer.hide()
+  }
+  if (tailPointer) {
+    tailPointer.hide()
+  }
+    
+  tailPointer = jsav.pointer("Tail", jsavArr.index(0), {arrowAnchor: "left top"})
+  headPointer = jsav.pointer("Head", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+})
+
+  // When a user clicks "pop" remove value at head of the array and update pointers
+popElement.addEventListener("click", function() {
+    jsavArr = poppedArray(jsav, jsavArr) 
+    
+    // Hide old pointers and create new pointers
+    if (headPointer) {
+      headPointer.hide()
+    }
+    if (tailPointer) {
+      tailPointer.hide()
+    }
+
+    tailPointer = jsav.pointer("Tail", jsavArr.index(0), {arrowAnchor: "left top"})
+    headPointer = jsav.pointer("Head", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
+})
+
 // Workaround since JSAV does not support adding to array datastructure
 function addToArray(jsav, jsavArray, value) {
     var newArr = jsavArray._values
@@ -15,54 +58,3 @@ function poppedArray(jsav, jsavArray) {
     jsavArray.hide()
     return jsav.ds.array(newArr)
 }
-
-
-function initiliaseStackVisualisation() {
-    // Initialise JSAV container and control buttons
-    let pushElement = document.getElementById("push");
-    let popElement = document.getElementById("pop");
-    let jsav = new JSAV("container");
-    let jsavArr = jsav.ds.array([]);
-    // Initialise JSAV pointers
-    let headPointer;
-    let tailPointer;
-
-
-    // When a user clicks "push" add new value to the array and update pointers
-    pushElement.addEventListener("click", function() {
-        let input = document.getElementById("userInput")
-        let valueToAdd = input.value
-        jsavArr = addToArray(jsav, jsavArr, valueToAdd)
-        
-        // Hide old pointers and create new pointers
-        if (headPointer) {
-          headPointer.hide()
-        }
-        if (tailPointer) {
-          tailPointer.hide()
-        }
-        
-        tailPointer = jsav.pointer("Tail", jsavArr.index(0), {arrowAnchor: "left top"})
-        headPointer = jsav.pointer("Head", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
-      })
-
-      // When a user clicks "pop" remove value at head of the array and update pointers
-      popElement.addEventListener("click", function() {
-          jsavArr = poppedArray(jsav, jsavArr) 
-         
-          // Hide old pointers and create new pointers
-          if (headPointer) {
-            headPointer.hide()
-          }
-          if (tailPointer) {
-            tailPointer.hide()
-          }
-  
-          tailPointer = jsav.pointer("Tail", jsavArr.index(0), {arrowAnchor: "left top"})
-          headPointer = jsav.pointer("Head", jsavArr.index(jsavArr.size() - 1), {arrowAnchor: "left top"})
-      })
-}
-initiliaseStackVisualisation()
-
-
-
